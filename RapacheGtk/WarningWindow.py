de94eea21d224caba98227f8c15ce899a09c508e
Sense Hofstede <sense@qense.nl>
2008-06-28 19:17:00 +0200

    Got the check for apache actually working.

--- RapacheGtk/WarningWindow.py
+++ RapacheGtk/WarningWindow.py
@@ -12,23 +12,25 @@ except:
     sys.exit(1)
     
 import os
+import gnome.ui
 
 from RapacheGtk import GuiUtils
+from RapacheCore import Configuration
 
 class WarningWindow:
 	"""The Rapache warning window"""
 
 	def __init__(self, warning, father = None):
 		self.father = father
-		self.gladefile = Configuration.GLADEPATH + "/" + "warning_dialog.glade"  
+		self.gladefile = Configuration.GLADEPATH + "/" + "warning_dialog.glade"
+		print self.gladefile
+		self.xml =  gtk.glade.XML(self.gladefile)
 		#Create our dictionary and connect it
 		dic = { 
-			"quit" : self.close
-			, "on_ok" : self.close
+			"quit" : gtk.main_quit
+			, "on_ok" : gtk.main_quit
 			}
 		self.xml.signal_autoconnect(dic)
 		self.xml.get_widget( 'label1' ).set_label ( warning )  
 		                  
-	def close ( self, widget = None ):
-		self.window = self.xml.get_widget( 'warning_dialog' )
-		self.window.destroy()
+
