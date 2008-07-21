170850116c04d6216a8054261b1931be23688d04
jason <jason@jason-laptop>
2008-07-22 08:46:25 +1200

    missing files

--- RapacheCore/PluginManager.py
+++ RapacheCore/PluginManager.py
@@ -5,28 +5,31 @@ import sys
 
 class PluginManager():
 
-	def __init__(self, pluginpath=None):
+	def __init__(self):
 	
 		print "-- Loading plugins --"
 		self.plugins = []
+		self.__add(os.path.join(sys.path[0], "plugins"))
+		self.__add(os.path.expanduser("~/.rapache/plugins"))
+		print ""
+		
+	def __add(self, pluginpath):
 
-		if pluginpath == None:
-			pluginpath = os.path.join(sys.path[0], "plugins")
-		print "pluging folder : " + pluginpath
+		print "checking plugin folder : " + pluginpath
 			
 		if not pluginpath in sys.path:
 			sys.path.insert(0, pluginpath)
 
-		for folder in os.listdir(pluginpath):
-			path = os.path.join(pluginpath,folder)
-			if os.path.isdir(path):				
-				try:
-					module = __import__(folder + ".plugin")
-					obj = module.plugin.register(path)
-					self.plugins.append(obj)
-					print "loaded plugin : " + folder
-				except:
-					print "error loading plugin " + folder
+		if os.path.exists(pluginpath):
+			for folder in os.listdir(pluginpath):
+				path = os.path.join(pluginpath,folder)
+				if os.path.isdir(path):				
+					try:
+						module = __import__(folder + ".plugin")
+						obj = module.plugin.register(path)
+						self.plugins.append(obj)
+						print "loaded plugin : " + folder
+					except:
+						print "error loading plugin " + folder
 		
-		print ""
-		print ""
+		
---
+++ plugins/advanced_vhost/plugin.py
@@ -0,0 +1,64 @@
+import os
+import gtksourceview
+
+try:
+     import pygtk
+     pygtk.require("2.0")
+except:
+      pass
+try:
+    import gtk
+    import gtk.glade
+except:
+    sys.exit(1)
+
+class AdvancedVhostPlugin:
+
+	def __init__(self, path):
+		
+		# The path to the plugin
+		self.path = path
+	
+		# module this plugin works with
+		self.module = ""
+		
+		# Define what additional config should be read from vhost file
+		self.vhosts_config = { "ServerAdmin" : 0 } # 0 value | 1 options
+		
+		# Get glade file XML
+		f = open( os.path.join(self.path, "vhost.glade") ,"r")
+		self.glade_vhost_xml =  f.read()
+		f.close()
+		
+		# Controls
+	
+	# Customise the module properties window
+	def load_module_properties(self, notebook, module):
+		return
+
+	# Perform action on module properties save
+	def save_module_properties(self, module):
+		return
+	
+	# Customise the vhost properties window
+	def load_vhost_properties(self, notebook, vhost_data):
+		label = gtk.Label("Advanced")
+		
+		# Remember you will need to recreate tree everytime the window loads
+		wtree = gtk.glade.xml_new_from_buffer(self.glade_vhost_xml, len(self.glade_vhost_xml), "table_advanced_vhost")
+		table_advanced_vhost = wtree.get_widget("table_advanced_vhost")	
+		
+		notebook.insert_page(table_advanced_vhost, label)
+		
+		# make sure to show items
+		label.show()
+		table_advanced_vhost.show()
+		return
+		
+	# Perform action on vhost properties save
+	def save_vhost_properties(self, vhost_data):
+		return
+
+def register( path ):
+	return AdvancedVhostPlugin( path )
+
