67bf39349c99ac22a9bd171f74c31c7c924e924f
tacone@tacone-macbook <>
2012-04-26 13:46:07 +0200

    relicensed apache parser under GPL and LGPL 3

--- RapacheCore/ApacheParser.py
+++ RapacheCore/ApacheParser.py
@@ -1,3 +1,18 @@
+#    This file is copyrighted by Stefano Forenza <stefano@stefanoforenza.com>
+#
+#    The code below is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License or (if you prefer) the 
+#    GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    The code below is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    To read the actual licenses see <http://www.gnu.org/licenses/>.
+
 import ApacheConf
 from lxml.builder import E
 from lxml import etree
--- tests/ApacheParserTest.py
+++ tests/ApacheParserTest.py
@@ -1,18 +1,18 @@
-# Rapache - Apache Configuration Tool
-# Copyright (C) 2008 Stefano Forenza,  Jason Taylor, Emanuele Gentili
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    This file is copyrighted by Stefano Forenza <stefano@stefanoforenza.com>
+#
+#    The code below is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License or (if you prefer) the 
+#    GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    The code below is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    To read the actual licenses see <http://www.gnu.org/licenses/>.
+
 import unittest
 import sys
 sys.path.append('../RapacheCore')
